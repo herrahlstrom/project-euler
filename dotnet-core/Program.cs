e23ecef145eeecd6c92906cc3d2b80d4336e0154
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-03-05 22:25:20 +0100

    Create .net core project
    And created a stub for problem 40

---
+++ dotnet-core/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var answer = new Problem40().Run();
+            Console.WriteLine("Answer: " + answer);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ dotnet-core/problem-40.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjectEuler
+{
+    public class Problem40
+    {
+        public string Run()
+        {
+            return "";
+        }
+    }
+}
