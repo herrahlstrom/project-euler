be7109373d01478ec88c2894b030aff83937da5c
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-03-06 12:24:56 +0100

    Solve problem 40

--- dotnet-core/problem-001-099/problem-40.cs
+++ dotnet-core/problem-001-099/problem-40.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 
 namespace ProjectEuler
 {
@@ -6,7 +6,41 @@ namespace ProjectEuler
     {
         public string Run()
         {
-            return "";
+            int d1 = 0, d10 = 0, d100 = 0, d1000 = 0, d10000 = 0, d100000 = 0, d1000000 = 0;
+            int i = 0;
+            foreach (var c in GetIrrationalDecimalFraction())
+            {
+                i++;
+                if (i == 1)
+                    d1 = ((int)c) - 48;
+                else if (i == 10)
+                    d10 = ((int)c) - 48;
+                else if (i == 100)
+                    d100 = ((int)c) - 48;
+                else if (i == 1000)
+                    d1000 = ((int)c) - 48;
+                else if (i == 10000)
+                    d10000 = ((int)c) - 48;
+                else if (i == 100000)
+                    d100000 = ((int)c) - 48;
+                else if (i == 1000000)
+                {
+                    d1000000 = ((int)c) - 48;
+                    break;
+                }
+            }
+            return (d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000).ToString();
+        }
+        private static IEnumerable<char> GetIrrationalDecimalFraction()
+        {
+            long i = 1;
+            while (true)
+            {
+                var s = i.ToString();
+                foreach (var c in s)
+                    yield return c;
+                i++;
+            }
         }
     }
 }
