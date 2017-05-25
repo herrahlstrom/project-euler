c0c9ae6690916b0f7658e266099dc11315983d51
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-05-25 20:08:21 +0200

    Change namespace

---
+++ dotnet-core/problems/problem42.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+	class problem42
+	{
+		public in
+		private IEnumerable<int> GetTriangleNumbers()
+		{
+			int n = 1;
+			while (true)
+			{
+				yield return (int)(0.5 * n * (n + 1));
+				n += 1;
+			}
+		}
+	}
+}
