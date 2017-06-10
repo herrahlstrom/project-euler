105d7ba4117a43eb96c1d0276ee14905f5c13321
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-10 23:06:17 +0200

    Add get_prime_factors
    And fix a bug in get_primes

--- python/prime.py
+++ python/prime.py
@@ -14,10 +14,10 @@ class Prime:
         return value in self.primes
         
     def get_primes(until):
-        a = [True] * until
+        a = [True] * (until+1)
         a[0] = False
         a[1] = False
-        for i in range(2, until):
+        for i in range(2, len(a)):
             if a[i]:
                 yield i
                 j = i * 2    
@@ -26,17 +26,9 @@ class Prime:
                     j += i
 
     def get_prime_factors(value):
-        
-        p1 = get_primes(5)
-        p2 = (get_primes(5))
-        p3 = [get_primes(5)]
-
-        primes = [get_primes(values)]
+        primes = list(Prime.get_primes(value))
         min = 0
-        while value > 1:
-            for i in range(min, len(primes)):
-                if value % primes[i] == 0:
-                    yield primes[i]
-                    value = value // primes[i]
-                else:
-                    min += 1
+        for p in primes:
+            while value % p == 0:
+                yield p
+                value = value // p
