26de30136222f61bc777a09ff079b75ac4e1a6c1
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-11 00:31:54 +0200

    Done with problem 47

--- python/main.py
+++ python/main.py
@@ -2,14 +2,23 @@
 
 def get_answer():
     p = Prime()
-    
-    p1 = Prime.get_primes(5)
-    p2 = (Prime.get_primes(5))
-    p3 = [Prime.get_primes(5)]
-    p4 = list(Prime.get_primes(5))
-
-    t = Prime.get_prime_factors(17);
-    return 5
+    in_row = 0
+    value = 2 * 3 * 5 * 7
+    while(in_row < 4):
+        c = 0
+        v = 0
+        # This assume the prime factors is in order
+        for pf in p.get_prime_factors(value):
+            if pf != v:
+                c +=1
+                v = pf
+        if c == 4:
+            in_row += 1
+            print(value, in_row)
+        else:
+            in_row = 0
+        value += 1
+    return value - in_row
 
 def main():
     print("Answer: {0}".format(get_answer()))
