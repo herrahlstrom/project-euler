ae9fadb6fe1cdaa4f0b19895326b531f66495dfa
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-11 00:05:58 +0200

    Minor changes

--- python/prime.py
+++ python/prime.py
@@ -1,20 +1,10 @@
 class Prime:
     def __init__(self):
-        self.primes = [2]
-        self.highestprime = 2
-
-    def is_prime(self, value):
-        if value == 1:
-            return True
-        if value < 1:
-            raise ValueError("Invalid number to test for prime")
-        if self.highestprime < value:
-            self.primes = list(Prime.get_primes(value * 3))
-            self.highestprime = self.primes[-1]
-        return value in self.primes
+        self.__primes = [2,3,5,7,11,13,17,19]
+        self.__primes_max = 19
         
     def get_primes(until):
-        a = [True] * (until+1)
+        a = [True] * (until + 1)
         a[0] = False
         a[1] = False
         for i in range(2, len(a)):
@@ -24,6 +14,17 @@ class Prime:
                 while j < len(a):
                     a[j] = False
                     j += i
+
+    def is_prime(self, value):
+        if value == 1:
+            return True
+        if value < 1:
+            raise ValueError("Invalid number to test for prime")
+        if self.__primes_max < value:
+            max = value * 3
+            self.__primes = list(Prime.get_primes(max))
+            self.__primes_max = max
+        return value in self.__primes
 
     def get_prime_factors(value):
         primes = list(Prime.get_primes(value))
