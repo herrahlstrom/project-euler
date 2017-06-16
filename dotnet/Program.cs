42b10db358a07ff83ed31ea6eef5da8d4da07d46
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-16 22:24:46 +0200

    Solved problem 47 to compare to python solution

---
+++ dotnet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace projecteuler
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string answer = GetAnswer();
+			Console.WriteLine("Answer: {0}", answer);
+		}
+		
+		private static string GetAnswer()
+		{
+			BigInteger sum = 0;
+			for (int value = 1; value <= 1000; value++)
+			{
+				sum += BigInteger.Pow(value, value);
+			}
+			string sumString = sum.ToString();
+			var answer = sumString.Substring(sumString.Length - 10);
+			return answer;
+		}
+	}
+}
