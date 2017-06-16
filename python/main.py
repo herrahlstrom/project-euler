65733721da761df86660425c87268c84243aa703
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-16 22:08:07 +0200

    Problem 48 solvet

--- python/main.py
+++ python/main.py
@@ -1,27 +1,13 @@
-﻿from prime import Prime
+﻿#from prime import Prime
+#from utils import ListUtil
 
 def get_answer():
-    p = Prime()
-    in_row = 0
-    value = 2 * 3 * 5 * 7
-    while(in_row < 4):
-        c = 0
-        v = 0
-        # This assume the prime factors is in order
-        for pf in p.get_prime_factors(value):
-            if pf != v:
-                c +=1
-                v = pf
-        if c == 4:
-            in_row += 1
-            print(value, in_row)
-        else:
-            in_row = 0
-        value += 1
-    return value - in_row
+   
+    sum = 0
+    for value in range(1,1001):
+        sum += value ** value
+    return str(sum)[-10:]
 
-def main():
-    print("Answer: {0}".format(get_answer()))
 
 if __name__ == "__main__":
-	main();
+	print("Answer: {0}".format(get_answer()))
