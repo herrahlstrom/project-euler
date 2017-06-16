4ec78355707d109c3c1b97db550c63651c597e7a
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-16 22:59:44 +0200

    Add docstrings to follow pep8

--- python/prime.py
+++ python/prime.py
@@ -1,36 +1,55 @@
+"""
+prime.py
+"""
+
+
 class Prime:
+    """
+    Prime number operations
+    """
+
     def __init__(self):
-        self.__primes = [2,3,5,7,11,13,17,19]
+        self.__primes = [2, 3, 5, 7, 11, 13, 17, 19]
         self.__primes_max = 19
-        
+
+    @staticmethod
     def get_primes(until):
-        a = [True] * (until + 1)
-        a[0] = False
-        a[1] = False
-        for i in range(2, len(a)):
-            if a[i]:
+        """
+        List all primes from 2 to a specified value
+        """
+        primemap = [True] * (until + 1)
+        primemap[0] = False
+        primemap[1] = False
+        for i in range(2, len(primemap)):
+            if primemap[i]:
                 yield i
-                j = i * 2    
-                while j < len(a):
-                    a[j] = False
+                j = i * 2
+                while j < len(primemap):
+                    primemap[j] = False
                     j += i
 
     def is_prime(self, value):
+        """
+        Detect if a specified value is a prime
+        """
         if value < 2:
             raise ValueError("Invalid number to test for prime")
         self.__ensure_primes(value)
         return value in self.__primes
 
     def get_prime_factors(self, value):
+        """
+        Get the prime factors for a specified value
+        """
         self.__ensure_primes(value)
-        for p in self.__primes:
-            while value % p == 0:
-                yield p
-                value = value // p
+        for prime in self.__primes:
+            while value % prime == 0:
+                yield prime
+                value = value // prime
             if value < 2:
                 break
 
     def __ensure_primes(self, until):
         if self.__primes_max < until:
-            self.__primes = list(Prime.get_primes(until * 2))
+            self.__primes = list(Prime.get_primes(self, until * 2))
             self.__primes_max = until * 2
--- python/utils.py
+++ python/utils.py
@@ -1,9 +1,21 @@
+"""
+utils.py
+"""
+
 class ListUtil:
-    def permute(input):
-        if len(input) <= 1:
-            yield input
+    """
+    Generic functions on lists
+    """
+
+    @staticmethod
+    def permute(inputlist):
+        """
+        Create all permutations of a given list (include strings)
+        """
+        if len(inputlist) <= 1:
+            yield inputlist
         else:
-            chunk = input[0]
-            for item in ListUtil.permute(input[1:]):
-                for i in range(0,len(item)+1):
+            chunk = inputlist[0]
+            for item in ListUtil.permute(inputlist[1:]):
+                for i in range(0, len(item)+1):
                     yield item[0:i] + chunk + item[i:]
