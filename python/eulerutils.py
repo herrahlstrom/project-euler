e470741f3e7b675c047f7630301c0cb33c980d51
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-25 20:43:11 +0200

    Optimized prime functions

--- python/eulerutils.py
+++ python/eulerutils.py
@@ -19,16 +19,20 @@ class Prime:
     Prime number operations
     """
 
-    def __init__(self):
-        self.__primes = [2, 3, 5, 7, 11, 13, 17, 19]
-        self.__primes_max = 19
+    def __init__(self, start=19):
+        self.__primes = list(Prime.__get_primes(start))
+        self.__primes_max = start
 
     def get_primes(self, until):
         """
         List all primes from 2 to a specified value
         """
-        self.__ensure_primes(until)
-        return [x for x in self.__primes if x <= until]
+        self.__ensure_primes(until * 2)
+        for primevalue in self.__primes:
+            if primevalue <= until:
+                yield primevalue
+            else:
+                break
 
     def is_prime(self, value):
         """
@@ -36,14 +40,14 @@ class Prime:
         """
         if value < 2:
             raise ValueError("Invalid number to test for prime")
-        self.__ensure_primes(value)
+        self.__ensure_primes(value * 2)
         return value in self.__primes
 
     def get_prime_factors(self, value):
         """
         Get the prime factors for a specified value
         """
-        self.__ensure_primes(value)
+        self.__ensure_primes(value * 2)
         for prime in self.__primes:
             while value % prime == 0:
                 yield prime
@@ -53,19 +57,14 @@ class Prime:
 
     def __ensure_primes(self, until):
         if self.__primes_max < until:
-            self.__primes = list(Prime.__get_primes(until * 2))
-            self.__primes_max = until * 2
+            self.__primes = list(Prime.__get_primes(until))
+            self.__primes_max = until
 
 
     @staticmethod
     def __get_primes(until):
-        primemap = [True] * (until + 1)
-        primemap[0] = False
-        primemap[1] = False
-        for i in range(2, len(primemap)):
-            if primemap[i]:
-                yield i
-                j = i * 2
-                while j < len(primemap):
-                    primemap[j] = False
-                    j += i
+        sieve = [True] * until
+        for i in range(3, int(until**0.5)+1, 2):
+            if sieve[i]:
+                sieve[i*i::2*i] = [False]*((until-i*i-1)//(2*i)+1)
+        return [2] + [i for i in range(3, until, 2) if sieve[i]]
