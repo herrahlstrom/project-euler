849631e4cd98e122e66cc6dc932000b5ca63a650
Martin Ahlström <herr.ahlstrom@gmail.com>
2017-06-25 21:27:56 +0200

    Add stopwatch

--- python/eulerutils.py
+++ python/eulerutils.py
@@ -2,6 +2,8 @@
 Generic helpers for Project Euler
 """
 
+from time import time
+
 def permute_list(inputlist):
     """
     Create all permutations of a given list (include strings)
@@ -27,7 +29,7 @@ class Prime:
         """
         List all primes from 2 to a specified value
         """
-        self.__ensure_primes(until * 2)
+        self.__ensure_primes(until)
         for primevalue in self.__primes:
             if primevalue <= until:
                 yield primevalue
@@ -40,14 +42,14 @@ class Prime:
         """
         if value < 2:
             raise ValueError("Invalid number to test for prime")
-        self.__ensure_primes(value * 2)
+        self.__ensure_primes(value)
         return value in self.__primes
 
     def get_prime_factors(self, value):
         """
         Get the prime factors for a specified value
         """
-        self.__ensure_primes(value * 2)
+        self.__ensure_primes(value)
         for prime in self.__primes:
             while value % prime == 0:
                 yield prime
@@ -68,3 +70,33 @@ class Prime:
             if sieve[i]:
                 sieve[i*i::2*i] = [False]*((until-i*i-1)//(2*i)+1)
         return [2] + [i for i in range(3, until, 2) if sieve[i]]
+
+class Stopwatch:
+    """
+    Create a stopwatch for taking elapsed times
+    """
+    def __init__(self):
+        self.elapsed = 0
+        self.current_start = None
+
+    def start(self):
+        """
+        Start the stopwatch
+        """
+        self.current_start = time()
+
+    def stop(self):
+        """
+        Stop the stopwatch
+        """
+        self.elapsed += time() - self.current_start
+        self.current_start = None
+
+    def get_elapsed(self):
+        """
+        Get the current elapsed time
+        """
+        if self.current_start is None:
+            return self.elapsed
+        return self.elapsed + time() - self.current_start
+        
